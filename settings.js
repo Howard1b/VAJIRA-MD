a85e706c49afb7c1c751b9e8023ed73d33bf4690
Howard1b <timm67594@gmail.com>
2024-07-21 12:28:44 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '263776312194' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=TAckXZCY#5GTj6vGAhU85qHHi97K2UAtjmXVtOmMlQ-EN0cXJhY8' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=mAlzmL6Z#HZMp5GSwYQNzy7-1u9l9i534W2_ncayArW6Vb5L4TE0' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
